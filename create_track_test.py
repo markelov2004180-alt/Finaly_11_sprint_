821a8f3d4383ed42981c3e13b1b9e89123e94bf5
markelov2004180-alt <markelov2004180@gmail.com>
2026-01-17 03:48:23 +0400

    В файл create_track_test добавил вызов функции создания заказа и указал, как запустить тест в README через Testing

--- create_track_test.py
+++ create_track_test.py
@@ -1,8 +1,14 @@
 # Маркелов Виталя, 39-я когорта — Финальный проект. Инженер по тестированию плюс
 import sender_stand_request
 
-track_number = sender_stand_request.response_data["track"]
 
 def test_create_number():
+
+    order_response = sender_stand_request.post_new_orders()
+    order_data = order_response.json()
+    track_number = order_data["track"]
+    
     response = sender_stand_request.get_track_orders(track_number)
     assert response.status_code == 200
+
+
